db0727f5c5a59295660f9fb77e896167d4d20127
Darshan <darshanparui7@gmail.com>
2026-06-10 13:56:26 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,374 @@
+#a = 12
+# b = 54
+# print(type(a), type(b))
+# print(a,b,sep=' $ ')
+
+
+
+
+# import keyword
+# print(keyword.kwlist)
+
+
+
+
+
+# a=int(input('Enter a number:'))
+# if a % 2 == 0:
+#     print('Even number:')
+
+
+# a=(input('Enter the character:'))
+# if a in 'aeiouAEIOU':
+#     print('It is a vowel:')
+
+
+
+# a=(input('Enter the character":'))
+# if ord(a)>=65 and ord(a)<=90:
+#     print('It is a upper character')
+
+
+
+# a=int(input('Enter the number:'))
+# if a % 3 == 0:
+#     print("It can be divisible")
+
+
+
+
+
+#Q. WaP to check the whether the given integer is 3 digit number.
+
+# a=int(input('Enter the number:'))
+# if a >= 0 and a <=9:
+#     print('It is an single digit number:')
+# else:
+#     print('It is an double digit number:')
+
+
+
+
+
+
+
+
+#Q. wap to check the whether the last digit  of a given number is 5
+
+# a=int(input('enter first number:'))
+# if a % 10 == 5:
+#     print('It is last digit number is 5:')
+# else:
+#     print('It is last digit number is not 5:')
+
+
+
+
+
+
+
+
+
+#Q. Wap to check the whether the given data is float
+
+# a=eval(input('Enter a data: '))
+# if type(a) == float:
+#     print('It is a float')
+# else:
+#     print('It is not a float')
+
+
+
+
+
+
+
+#Q. Wap to check the whether the data is single value data
+
+# a=eval(input('Enter the data:'))
+# if type(a)== int or type(a)== float or type(a)== complex or type(a)== bool:
+#     print('It is a single value data:')
+
+
+
+
+
+
+
+
+#Q. Wap to check whether the given character is digit or not
+
+# a=(input('Enter the character:'))
+# if '0' <= a <= '9':
+#     print('It is digit ' )
+
+
+
+
+
+
+
+#Q. Wap to check whether the given integer is multiple of 3
+
+# a=int(input('Enter the number:'))
+# if a % 3 == 0:
+#        print('It is multiple of 3')
+
+
+
+
+
+
+
+#Q. Wap to check whether the data is mutable or not
+
+# a=eval(input('Enter the number:'))
+# if type(a) in [int,float,complex,bool,str,tuple]:
+#     print('It is a immutable  datatype')
+# else:
+#     print('It is a mutable datatype:')
+
+
+
+
+
+
+#Q. wap to check whether the given character is digit  or  not
+
+# a=(input('Enter the character:'))
+# if '0' <= a <= '9':
+#     print('It is digit')
+# else:
+#     print('It is not digit:')
+
+
+
+
+
+
+
+#Q. Wap to check whether the given character is special or not
+
+
+# a=(input('Enter the special character:'))
+# if 'A' <= a <= 'Z' or '0' <= a <= '9' or 'a' <= 'z':
+#     print('It is not a special character')
+# else:
+#     print('It is a special character')
+
+
+
+
+
+
+
+#Q. Wap to check whether a list consists of middle value or not
+
+# a=eval(input('Enter the list:'))
+# if  len(a)% 2 == 0:
+#     print('It have not a middle value')
+# else:
+#     print('It have a middle value')
+#a = 12
+# b = 54
+# print(type(a), type(b))
+# print(a,b,sep=' $ ')
+
+
+
+
+# import keyword
+# print(keyword.kwlist)
+
+
+
+
+
+# a=int(input('Enter a number:'))
+# if a % 2 == 0:
+#     print('Even number:')
+
+
+# a=(input('Enter the character:'))
+# if a in 'aeiouAEIOU':
+#     print('It is a vowel:')
+
+
+
+# a=(input('Enter the character":'))
+# if ord(a)>=65 and ord(a)<=90:
+#     print('It is a upper character')
+
+
+
+# a=int(input('Enter the number:'))
+# if a % 3 == 0:
+#     print("It can be divisible")
+
+#Q. WaP to check the whether the given integer is 3 digit number.
+# a=int(input('Enter the number:'))
+# if a >= 0 and a <=9:
+#     print('It is an single digit number:')
+# else:
+#     print('It is an double digit number:')
+
+
+#Q. wap to check the whether the last digit  of a given number is 5
+# a=int(input('enter first number:'))
+# if a % 10 == 5:
+#     print('It is last digit number is 5:')
+# else:
+#     print('It is last digit number is not 5:')
+
+
+#Q. Wap to check the whether the given data is float
+
+# a=eval(input('Enter a data: '))
+# if type(a) == float:
+#     print('It is a float')
+# else:
+#     print('It is not a float')
+
+
+
+#Q. Wap to check the whether the data is single value data
+
+# a=eval(input('Enter the data:'))
+# if type(a)== int or type(a)== float or type(a)== complex or type(a)== bool:
+#     print('It is a single value data:')
+
+
+#Q. Wap to check whether the given character is digit or not
+
+# a=(input('Enter the character:'))
+# if '0' <= a <= '9':
+#     print('It is digit ' )
+
+#Q. Wap to check whether the given integer is multiple of 3
+
+# a=int(input('Enter the number:'))
+# if a % 3 == 0:
+#        print('It is multiple of 3')
+
+#Q. Wap to check whether the data is mutable or not
+
+# a=eval(input('Enter the number:'))
+# if type(a) in [int,float,complex,bool,str,tuple]:
+#     print('It is a immutable  datatype')
+# else:
+#     print('It is a mutable datatype:')
+
+#Q. wap to check whether the given character is digit  or  not
+
+# a=(input('Enter the character:'))
+# if '0' <= a <= '9':
+#     print('It is digit')
+# else:
+#     print('It is not digit:')
+
+#Q. Wap to check whether the given character is special or not
+# a=(input('Enter the special character:'))
+# if 'A' <= a <= 'Z' or '0' <= a <= '9' or 'a' <= 'z':
+#     print('It is not a special character')
+# else:
+#     print('It is a special character')
+
+
+#Q. Wap to check whether a list consists of middle value or not
+
+# a=eval(input('Enter the list:'))
+# if  len(a)% 2 == 0:
+#     print('It have not a middle value')
+# else:
+#     print('It have a middle value')
+
+
+#Q.Consider a tuple of length  2 and check whether the tuple is homogenous or not
+
+# a=eval(input('Enter the number: '))
+# if type(a[0]) == type(a[1]):
+#     print('It is homogenus')
+# else:
+#     print('It is not a homogenenus')
+
+
+#Q, Wap to check whether the string is palindrome or not
+
+# a=(input('Enter the character:'))
+# if a==a[::-1]:
+#     print(a,'It is palindrome')
+# else:
+#     print('It is not palindrome')
+
+
+#Q. Possitive and negative
+
+# a=int(input('Enter the number:'))
+# if a >= 0 :
+#     print('It is possitive')
+# else:
+#     print('Negative')
+
+#Q. Wap to check if the given number is single digit number or double digit number or three digit number or greater then three digit number
+
+
+# a=int(input('Enter the number:'))
+# if 0 <= a <= 9:
+#     print('It is single digit number')
+# elif 10 <= a <= 99:
+#     print('It is double digit number:')
+# elif 100 <= a <= 999:
+#     print('It is three digit number:')
+# elif 1000 <= a <= 9999:
+#     print('It is four digit number:')
+# else:
+#     print('It is greater then four number')
+
+#Q. Wap to find the greatest of three number
+
+# a=int(input('Enter the number:'))
+# b=int(input('Enter the number:'))
+# c=int(input('Enter the number:'))
+# if a > b and a > c:
+#     print('a is greatest')
+# elif b >a and b > c:
+#     print('b is greatest')
+# elif c > a and c > b:
+#     print('c is greatest')
+# else:
+#     print('Every number is equal')
+
+
+#Q.  Wap to print grade of the student based on the marks
+    # if marks are > 75 then grade 1
+    # if marks are between 35 to 75 then grade 2
+    # if marks are lesser than 35 than grade 3
+
+# a=int(input('Enter the grade:'))
+# if a > 75:
+#     print('Grade is 1')
+# elif a>=35  and a<=75:
+#     print('Grade is 2')
+# elif a < 35:
+#     print('Grade is 3')
+
+
+
+
+#Q. Consider a character input if it is uppercase convert it into lower case it into lower
+#   case, if it is lower convert it into upper case, if it is digit print the reminder when it
+#   is divied by 3 else if it is special charcter print it's ASCII value
+
+a=(input('Enter the character: '))
+if 'A' <= a <= 'Z':
+    print(a.swapcase(),'Upper to lower')
+elif 'a' <= a <= 'z':
+    print(a.swapcase(),'Lower to upper')
+elif '0' <= a <= "9":
+    print(int(a)%3,'Reminder of digit')
+else:
+    print(ord(a),'ASCII value')
+
+
+
+#Q. Wap to print 'Fizz' if the given number is multiple of three print '
