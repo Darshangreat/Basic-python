8127cbd5e85d77ed78858136d3b34d63d8b178b7
Darshan <darshanparui7@gmail.com>
2026-06-17 11:58:51 +0530

    Add files via upload

---
+++ Basic For.py
@@ -0,0 +1,44 @@
+# for i in 'hii':
+#     print(i)
+
+# for i in [12,45,89]:
+#     print(i)
+
+
+# for i in ('hii',12,54,4+4j):
+#     print(i)
+
+# for i in {12,'hi',12.3,4+8j,True}:
+#     print(i)
+
+# b = {'name': 'Darshan', 'age': 12}
+# for i in b:
+#     print(i, b[i])
+
+
+
+
+
+
+#Q. Wap to print only the numbers divisible by 5 between 100 to 300
+
+
+
+# for i in range(100,300 + 1):
+#     if  i % 5 == 0:
+#         print(i)
+
+
+#Q Wap to print only vowels from the string
+
+# a=input('Enter the string:')
+
+# i = 0
+# while i < len(a):
+#     if a[i] in 'AEIOUaeiou':
+#         print(a[i])
+#     i += 1
+
+# for i in a:
+#     if i in 'AEIOUaeiou':
+#         print(i)
