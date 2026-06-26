992f100ad1bc5e10163e8c8a19cb01d8aa6979c7
Darshan <darshanparui7@gmail.com>
2026-06-26 14:06:04 +0530

    Add files via upload

--- Dictionery program.py
+++ Dictionery program.py
@@ -42,7 +42,7 @@
 
 #Q. Wap to get the following output
 
-a=['Python','Hii','holiday','Byee']
+# a=['Python','Hii','holiday','Byee']
 
 # output={'Python':6,'Hii': 3,'Holiday': 7,'Byee': 4}
 
@@ -53,13 +53,135 @@ a=['Python','Hii','holiday','Byee']
 # print(out)
 
 
+
+
+
 # output={'Python':6,'Hii':'iiH','holiday':'yadiloh','Byee':4}
+
+# out = {}
+# for i in a:
+#     if len(i) % 2 == 0:
+#         out[i] = len(i)
+#     else:
+#         out[i] = i[::-1]
+#
+# print(out)
+
+
+
+
+
+
+
+#Q. Wap to count occurences of the character in the string
+
+
+#agar nhi toh add karo or agar hai toh update karo
+
+# a='banana'
+# out = {}
+# for i in a:
+#     if i not in out:
+#         out[i] = 1
+#     else:
+#         out[i] = out[i] + 1
+# print(out)
+
+
+
+#Q.
+
+# a='Python is easy Python is dynamically typed'
+#output={'Python':2,'is':2,'easy':1,'dynamically':1,'typed':1}
+
+
+# out = {}
+# data=a.split()
+#
+# for i in data:
+#     if i not in out:
+#         out[i] = 1
+#     else:
+#         out[i] = out[i] + 1
+# print(out)
+
+
+
+#Q.
+
+# a=(12,3,4,'hello',2+3j,'python','bye',False)
+#output={'hello : 5,'python' : 6, 'bye' : 3}
+
+# out = {}
+#
+# for i in a:
+#     if type(i) == str:
+#         out[i] = len(i)
+# print(out)
+
+
+
+
+
+#Q.
+
+# a=(12,3,4,'hello',2+3j,'python','bye',False)
+# output={'hello':'ho','python':'pn','bye':'be'}
+
+# out ={}
+# for i in a:
+#     if type(i) == str:
+#         out[i] = i[0] + i[-1]
+# print(out)
+
+
+
+
+
+#Q. Wap to replace the space with underscore in the below string
+
+# a='Python is easy Python is dynamically typed'
+
+# out = ''
+#
+# for i in a:
+#     if i == ' ':
+#         out = out + '_'
+#     else:
+#         out = out + i
+# print(out)
+
+
+
+#Q.
+
+# a=['python.py','basic.html','loop.py','st.css']
+# out=['py','html','py','css']
+
+# out = []
+#
+# for i in a:
+#     data = i.split('.')
+#     out.append(data[1])
+#     print(i)
+# print(out)
+
+
+
+
+
+#.Q
+
+a=['Python.py','basic.html','loop.py','st.css','normal.html','static.py']
+
+# output={'py':3,'html':2,'css':1}
 
 out = {}
 for i in a:
-    if len(i) % 2 == 0:
-        out[i] = len(i)
+    data = i.split('.')
+    key=data[1]
+    if key not in out:
+        out[key] = 1
     else:
-        out[i] = i[::-1]
-
+        out[key] = out[key] + 1
 print(out)
